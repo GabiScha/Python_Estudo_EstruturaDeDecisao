5bb54f8f2ef23a4bdaf931b2970f6a21fef9303f
Gabriela Schaper <gabrielasch2409@gmail.com>
2025-02-17 09:51:25 -0300

    09

---
+++ Files/09.py
@@ -0,0 +1,11 @@
+#9- Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
+num1 = int(input("Escreva o primeiro numero: "))
+num2 = int(input("Escreva o primeiro numero: "))
+num3 = int(input("Escreva o primeiro numero: "))
+
+numeros = [num1, num2, num3]
+numeros.sort()
+print(numeros)
+
+
